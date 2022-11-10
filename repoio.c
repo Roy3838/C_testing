234bb9892e757ba5a369e452eb26484c559653e2
Roy3838 <a01283783@itesm.mx>
2022-11-10 16:42:50 -0600

    cambios de la carpeta de estocasticos

--- repoio.c
+++ repoio.c
@@ -1,71 +1,110 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
+#include <time.h>
 
-int roy_uniform(int low, int high, int iterations) {
-    double a = pow(7,5);
-    double m = pow(2,31)-1;
 
-    double x_0 = fmod(470211272 /(a),m);
-    double x[iterations];
-    x[0] = x_0;
-    double u_x[iterations];
-    u_x[0] = x_0;
+double roy_uniform(#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <time.h>
 
-    //Se generan los numeros aleatorios uniformes
-    for(int i = 1; i < iterations; i++) {
-        x[i] = fmod(a*x[i-1],m);
-        u_x[i] = (double)x[i]/m;
-    }
-    // escalar entre low y high
-    u_x[iterations] = low + (high-low)*u_x[iterations];
 
-    return u_x[iterations];
+double roy_uniform(double a, double b, double decimales){
+    /*#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <time.h>
+
+
+double roy_uniform(double a, double b, double decimales){
+    /*ouble a, double b, double decimales){
+    /*
+    Funcion que genera un numero aleatorio uniforme entre a y b
+    con una cantidad de decimales dada.
+    */
+    b = b*pow(10,decimales);
+    a = a*pow(10,decimales);
+    double r = rand() % (int)(b-a) + a;
+    r=r/pow(10,decimales);
+    return r;
+
 }
 
 double f_x(double x, double y){
-    double primerarg= pow(x,2) + pow(y,2);
-    double segundoarg = pow(sin(x),2) + pow(sin(y),2);
+
+    int d = 0;
+    double primerarg= pow(x+d,2) + pow(y+d,2);
+    double segundoarg = pow(sin(x+d),2) + pow(sin(y+d),2);
     return  primerarg + 25*segundoarg;
+
 }
+
 
 int main(){
     // Simulated Annealing
     double seed = 10;
-    int range= 10;
-    double x_0 = rand()%range -5;
-    double y_0 = rand()%range -5;
+    int range= 100;
+    clock_t start, end;
+    double cpu_time_used;
+    // change rand() seed dependent of time
+    srand(time(NULL));
+    double x_0 = roy_uniform(-50,50,2);
+    double y_0 = roy_uniform(-50,50,2);
     double p_0[] = {x_0,y_0};
-    double T_0 = 100;
-    double T_min = 0.01;
+    double T_0 = 389;
+    double T_min = 0.00001;
     double p_1[] = {p_0[0] ,p_0[1]};
-    int L = 100;
+    int resolucion = 1000;
+    
+    // guardar la trayectoria
+    double x[100000];
+    double y[100000];
+    int contador = 0;
+
+    int L = 1000;
+    printf("El inicial es: %f , %f \n",p_0[0],p_0[1]);
+    start = clock();
+
     while(T_0 > T_min){
-        T_0=0.9*T_0;
-        printf("El minimo es: %f , %f \n",p_0[0],p_0[1]);
+        T_0=0.875*T_0;
         for(int i = 0; i<L; i++){
-            double x_1 = (rand()%100)*0.01;
-            double y_1 = (rand()%100)*0.01;
+            // double x_1 = (rand()%10000)*0.0001 - 0.5; // = uniform(-0.5,0.5) pero con 0.01 
+            // double y_1 = (rand()%10000)*0.0001 - 0.5;
+            double x_1 = roy_uniform(-0.5,0.5,3);
+            double y_1 = roy_uniform(-0.5,0.5,3);
             double p_1[] = {p_0[0] + x_1 ,p_0[1] + y_1};
-            if (f_x(p_1[0],p_1[1]) < f_x(p_0[0],p_0[1])){
+            double valor_p_1 = f_x(p_1[0],p_1[1]);
+            double valor_p_0 = f_x(p_0[0],p_0[1]);
+            contador++;
+            if (valor_p_1 < valor_p_0){
                 p_0[0] = p_1[0];
                 p_0[1] = p_1[1];
             }
-            // else{
-            //     double r = (rand()%100)*0.01;
-            //     double e = exp((f_x(p_0[0],p_0[1])-f_x(p_1[0],p_1[1]))/T_0);
-            //     if (r < e){
-            //         p_0[0] = p_1[0];
-            //         p_0[1] = p_1[1];
-            //     }
-            // }
+            else{
+                double r = roy_uniform(0,1,2); // numero aleatorio entre 0 y 1
+                double e = exp((valor_p_0 - valor_p_1)/T_0); // e^(-deltaE/T)
+                if (r < e){ // si el numero aleatorio es menor que e^(-deltaE/T)
+                    p_0[0] = p_1[0]; // se acepta el nuevo punto
+                    p_0[1] = p_1[1];
+                }
+            }
 
             }
 
-
-
-
         }
+    end = clock();
+    cpu_time_used = ((double) (end - start)) / CLOCKS_PER_SEC;
+    printf("%f seconds \n",cpu_time_used);
+
+    printf("Cero en : %f , %f \n",p_0[0],p_0[1]);
+    printf("iteraciones : %d \n",contador);
+
+    
+
+    
+    
+
 
 
     return 0;
